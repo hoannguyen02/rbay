7edf7e97ea62ba42c81c5cca31d59ecabcec900f
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2023-10-13 22:40:00 +0700

    Add queries for item

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,2 +1,3 @@
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionKey = (sessionId: string) => `session#${sessionId}`;
+export const itemsKey = (itemId: string) => `items#${itemId}`;
