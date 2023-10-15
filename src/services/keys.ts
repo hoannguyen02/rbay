4a7305862a932479db59edb6ac728f2fe46059a8
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2023-10-15 23:31:28 +0700

    Add like items for specific user using set

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -2,3 +2,4 @@ export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionKey = (sessionId: string) => `session#${sessionId}`;
 export const itemsKey = (itemId: string) => `items#${itemId}`;
 export const usernamesUniqueKey = () => 'usernames#unique';
+export const userLikesKey = (userId: string) => `user:likes#${userId}`;
