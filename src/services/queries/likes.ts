62aa0db9910b4704b46956602b246a8abb648075
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2023-10-15 23:49:24 +0700

    Count liked item using hIncBy

--- src/services/queries/likes.ts
+++ src/services/queries/likes.ts
@@ -1,4 +1,4 @@
-import { userLikesKey } from '$services/keys';
+import { itemsKey, userLikesKey } from '$services/keys';
 import { client } from '$services/redis';
 
 export const userLikesItem = async (itemId: string, userId: string) => {
@@ -8,11 +8,17 @@ export const userLikesItem = async (itemId: string, userId: string) => {
 export const likedItems = async (userId: string) => {};
 
 export const likeItem = async (itemId: string, userId: string) => {
-	await client.sAdd(userLikesKey(userId), itemId);
+	const liked = await client.sAdd(userLikesKey(userId), itemId);
+	if (liked) {
+		return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+	}
 };
 
 export const unlikeItem = async (itemId: string, userId: string) => {
-	await client.sRem(userLikesKey(userId), itemId);
+	const unlike = await client.sRem(userLikesKey(userId), itemId);
+	if (unlike) {
+		return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+	}
 };
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
