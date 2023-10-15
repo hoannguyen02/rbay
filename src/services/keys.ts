53e4995c54416ad8d6d7edadd7269b0b89ed2772
Hoan Nguyen <nguyenvanhoan02@gmail.com>
2023-10-15 10:56:51 +0700

    Validate username is taken using set

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,3 +1,4 @@
 export const usersKey = (userId: string) => `users#${userId}`;
 export const sessionKey = (sessionId: string) => `session#${sessionId}`;
 export const itemsKey = (itemId: string) => `items#${itemId}`;
+export const usernamesUniqueKey = () => 'usernames#unique';
